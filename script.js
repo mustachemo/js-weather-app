4ba485b4fe51a29d301e44d71ad5f096571d59b8
moproblems8 <mhasan1080@gmail.com>
2023-07-28 18:32:55 -0700

    attempting to fix proxy issue

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ const curretWeatherEndpoint = '/current.json';
 
 const weatherAPIKey = 'faab396b2645438bbdf04155232907';
 
-function getCurrentWeather(query) {
+async function getCurrentWeather(query, aqi = 'no') {
 	const modifiedQuery = query.replace(/ /g, '%20');
+	finalURl =
+		weatherAPIbase +
+		curretWeatherEndpoint +
+		'?key=' +
+		weatherAPIKey +
+		'&q=' +
+		modifiedQuery +
+		'&aqi=' +
+		aqi;
+
+	const reseponse = await fetch(finalURl);
 }
+
+getCurrentWeather('92688');
