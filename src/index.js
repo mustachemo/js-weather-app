eb22f7c5ee6a63f334ee6cfbb184ba3b8a83f035
moproblems8 <mhasan1080@gmail.com>
2023-07-28 20:20:24 -0700

    added eslint and prettier config

--- src/index.js
+++ src/index.js
@@ -10,15 +10,9 @@ const weatherAPIKey = 'faab396b2645438bbdf04155232907';
 
 async function getCurrentWeather(query, aqi = 'no') {
 	const modifiedQuery = query.replace(/ /g, '%20');
-	const finalURl =
-		weatherAPIbase +
-		curretWeatherEndpoint +
-		'?key=' +
-		weatherAPIKey +
-		'&q=' +
-		modifiedQuery +
-		'&aqi=' +
-		aqi;
+	const finalURl = `${
+		weatherAPIbase + curretWeatherEndpoint
+	}?key=${weatherAPIKey}&q=${modifiedQuery}&aqi=${aqi}`;
 
 	const response = await fetch(finalURl, { mode: 'cors' });
 	const weatherData = await response.json();
